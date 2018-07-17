80cc94da5409b73df7a7994035dadcc094f48a85
Ryan Faulhaber <faulhaberryan@gmail.com>
2018-07-16 20:48:56 -0400

    added some structs

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	// use when drawing progress bars!
+		// use when drawing progress bars!
 	//"gopkg.in/cheggaaa/pb.v1"
 	"encoding/json"
+	"github.com/ogier/pflag"
 	"io/ioutil"
-)
+	)
 
 type SFConfig struct {
 	Username     string `json:"username"`
@@ -16,36 +15,56 @@ type SFConfig struct {
 	InstanceName string `json:"instanceId"`
 }
 
-type SFAuth struct {
+type SFSession struct {
 	SessionID string
 	ServerURL string
 }
 
-var actionMap = map[string]struct{}{
+type Operation int
+
+const (
+	Insert = iota
+	Update
+	Delete
+	Upsert
+)
+
+var operations = []string{
+	"insert",
+	"update",
+	"delete",
+	"upsert",
+}
+
+var operationMap = map[string]struct{}{
 	"insert": {},
 	"update": {},
 	"delete": {},
 	"upsert": {},
 }
 
-type Command struct {
+func (o Operation) String() string {
+	return operations[o]
+}
+
+type Batch struct {
 	Action string
 	Files  []string
 }
 
 type Process struct {
-	Commands []Command
+	Batches []Batch
 
 	// if false, serial
 	IsParallel bool
 }
 
-func (p Process) OptimizeCommands() []Command {
+func (p Process) OptimizeBatches() []Batch {
 	m := p.CommandMap()
-	cmds := make([]Command, 0)
+	cmds := make([]Batch, 0)
 
 	for action, files := range m {
-		cmds = append(cmds, Command{action, files})
+		cmds = append(cmds, Batch{action, files})
 	}
 
 	return cmds
@@ -58,26 +77,50 @@ func (p Process) OptimizeCommands() []Command {
 func (p Process) CommandMap() map[string][]string {
 	m := make(map[string][]string)
 
-	for _, cmd := range p.Commands {
-		for _, file := range cmd.Files {
-			m[cmd.Action] = append(m[cmd.Action], file)
+	for _, b := range p.Batches {
+		for _, file := range b.Files {
+			m[b.Action] = append(m[b.Action], file)
 		}
 	}
 
 	return m
 }
 
-type Job struct {
-	C      chan JobInfo
-	Done   chan bool
+type ConcurrencyMode int
 
-	auth SFAuth
+const (
+	Parallel = iota
+	Serial
+)
+
+var concurrencyModes = []string{
+	"Parallel",
+	"Serial",
+}
+
+func (c ConcurrencyMode) String() string {
+	return concurrencyModes[c]
+}
+
+func (c ConcurrencyMode) MarshalJSON() ([]byte, error) {
+	str := concurrencyModes[c]
+	return []byte(`"` + str + `"`), nil
+}
+
+type Job struct {
+	C    chan JobInfo
+	Done chan bool
+
+	session         SFSession
+	batches         []Batch
+	concurrencyMode ConcurrencyMode
 }
 
 type JobRequest struct {
-	Operation   string `json:"operation"`
-	Object      string `json:"object"`
-	ContentType string `json:"contentType"`
+	Operation       string `json:"operation"`
+	Object          string `json:"object"`
+	ContentType     string `json:"contentType"`
+	ConcurrencyMode string `json:"concurrencyMode"`
 }
 
 type JobInfo struct {
@@ -103,18 +146,35 @@ type JobInfo struct {
 	TotalProcessingTime     int     `json:"totalProcessingTime"`
 }
 
+// sends request to server to create job
 func (j *Job) Create() {
-
 }
 
+// adds a batch to the object, to be sent to the server later
+func (j *Job) AddBatch(b Batch) {
+	j.batches = append(j.batches, b)
+}
+
+func (j *Job) SetAuth(session SFSession) {
+	j.session = session
+}
+
+// Job defaults to "Parallel", set this to make it "Serial" instead
+func (j *Job) SetSerialMode() {
+	j.concurrencyMode = Serial
+}
+
+// sends all batches to the server
 func (j *Job) Run() {
 }
 
-func NewJob(auth SFAuth) *Job {
+func NewJob(object string, operation string) *Job {
 	return &Job{
 		make(chan JobInfo),
 		make(chan bool),
-		auth,
+		SFSession{},
+		make([]Batch, 0),
+		Parallel,
 	}
 }
 
@@ -128,30 +188,32 @@ func main() {
 	 * - out flag (for writing / piping output)
 	 * - flag to indicate program should convert non-CSV to CSV
 	 */
-	syncFlag := flag.Bool("s", false, "If true, does data load in sync mode")
+	//syncFlag := pflag.BoolP("sync", "s", false, "If true, does data load in sync mode")
 
-	flag.Parse()
+	pflag.Parse()
 
-	cmds := parseArgs(flag.Args())
+	//cmds := parseArgs(pflag.Args())
+	//
+	//process := Process{cmds, *syncFlag}
+	//
+	//fmt.Println(process)
+	//fmt.Println(process.CommandMap())
 
-	process := Process{cmds, *syncFlag}
-
-	fmt.Println(process)
-	fmt.Println(process.CommandMap())
+	// TODO add waitgroup while everything is processing
 }
 
-func parseArgs(args []string) []Command {
+func parseArgs(args []string) []Batch {
 	// TODO handle invalid syntax
-	cmds := make([]Command, 0)
+	cmds := make([]Batch, 0)
 
 	for len(args) > 0 {
-		cmd := Command{}
+		cmd := Batch{}
 
 		cmd.Action = args[0]
 
 		i := 1
 
-		for i < len(args) && !isCommand(args[i]) {
+		for i < len(args) && !isOperation(args[i]) {
 			cmd.Files = append(cmd.Files, args[i])
 			i++
 		}
@@ -175,7 +237,7 @@ func loadConfigFile(path string) (config SFConfig, err error) {
 	return
 }
 
-func isCommand(str string) bool {
-	_, ok := actionMap[str]
+func isOperation (str string) bool {
+	_, ok := operationMap[str]
 	return ok
 }
